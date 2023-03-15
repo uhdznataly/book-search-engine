7bfde792e7583f43b488d78cfef42f539e38c0bf
uhdznataly <112514096+uhdznataly@users.noreply.github.com>
2023-03-14 21:26:37 -0500

    add readme

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -15,7 +15,7 @@ import { REMOVE_BOOK } from "../utils/mutations";
 
 const SavedBooks = () => {
   const { loading, data } = useQuery(GET_ME);
-  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
+  const [removeBook] = useMutation(REMOVE_BOOK);
   const userData = data?.me || {};
 
   const handleDeleteBook = async (bookId) => {
